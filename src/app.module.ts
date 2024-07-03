1a595be7019ed20983f1908b0e84506daf5c69ab
rikwilisbr <henriquewilliamstos@gmail.com>
2024-07-02 21:27:51 -0300

    fix: delete auth module import

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from 'prisma/prisma.module';
 import { PostsModule } from './posts/posts.module';
 import { NewsletterModule } from './newsletter/newsletter.module';
 
 @Module({
-  imports: [AuthModule, PrismaModule, PostsModule, NewsletterModule],
+  imports: [PrismaModule, PostsModule, NewsletterModule],
 })
 export class AppModule {}
